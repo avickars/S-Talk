ce546edb9b8499713c639308ed4557f95265309b
snickers2524 <aidanvickars@gmail.com>
2020-06-27 15:05:09 -0700

    workshop work

--- s-talk.c
+++ s-talk.c
@@ -1,42 +1,43 @@
-#include <stdlib.h>	/* defines system calls */
-#include <stdio.h>	/* needed for printf */
-#include <string.h>	/* needed for memset */
-#include <sys/socket.h>
-#include <netinet/in.h>	/* needed for sockaddr_in */
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    struct sockaddr_in myaddr;	/* our address */
-	int fd;	/* our socket */
-	unsigned int alen;	/* length of address (for getsockname) */
-        
-    /* create a udp/ip socket */
-    /* request the Internet address protocol */
-    /* and a datagram interface (UDP/IP) */
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("cannot create socket");
-        return 0;
-    }
-    printf("created socket: descriptor=%d\n", fd);
+#define MSG_MAX_LEN 1024
+#define PORT 22110
+
+int main() {
+	printf("Aidan Net Listen Test on UDP Port %d:\n",PORT);
+	printf("Connect using: \n");
+	printf("netcate -u 127.0.0.1 %d\n",PORT);
+	
+	struct sockaddr_in sin;
+	memset(&sin, 0 ,sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_port = htons(PORT);
+
+	int socketDescriptor = socket(PF_INET, SOCK_DGRAM, 0);
+
+	bind(socketDescriptor, (struct sockaddr*) &sin, sizeof(sin));
+
+	while (1) {
+		struct sockaddr_in sinRemote;
+		unsigned int sin_len = sizeof(sinRemote);
+		char messageRx[MSG_MAX_LEN];
+		int bytesRx = recvfrom(socketDescriptor,
+		messageRx, MSG_MAX_LEN, 0,
+		(struct sockaddr *) &sinRemote, &sin_len);
+
+		int terminateIdx = (bytesRx < MSG_MAX_LEN) ? bytesRx: MSG_MAX_LEN - 1;
+
+		printf(">> %s ", messageRx);
 
 
-    memset((void *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(0);
-
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		return 0;
 	}
-
-	alen = sizeof(myaddr);
-	if (getsockname(fd, (struct sockaddr *)&myaddr, &alen) < 0) {
-		perror("getsockname failed");
-		return 0;
-	}
-
-	printf("bind complete. Port number = %d\n", ntohs(myaddr.sin_port));
-     
+	
+   
 
     return 0;
 }
