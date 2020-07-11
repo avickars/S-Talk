4ae2c5401d8c13753aa48f65c15547a594b9ab8f
aidan <aidanvickars@gmail.com>
2020-07-10 18:58:15 -0700

    added sender cleanup routine

--- sender.c
+++ sender.c
@@ -2,27 +2,38 @@
 #include <stdio.h> // For printf
 #include <stdlib.h> // For free
 #include <string.h> // For memset
-#include "input.h"
-#include "list.h"
-
-#include "unistd.h" // For sleep
-
-#include "display.h"
-#include "receiver.h"
-
-
 #include <netdb.h> // For socket
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include "list.h" // List implementation
+
+#include "input.h"
+#include "display.h"
+#include "receiver.h"
+
+
+
+
 #define MSG_MAX_LEN 1024
+
 pthread_t senderThread; // Defining the Sender thread
+
+char *messageToSend; // Pointer to the message to be sent
 
 typedef struct senderArgs_s senderArgs;
 struct  senderArgs{
  	void *REMOTENAME;
 	void *REMOTEPORT;
 };
+
+void senderCleanup(void *socketDescriptor) {
+    shutdown(*(int *)socketDescriptor, 2);
+    free(messageToSend);
+    pthread_cond_signal(&inputSpotAvailable);
+    pthread_mutex_unlock(&acceptingInputMutex);
+
+}
 
 void *sender(void *args) {
 	struct senderArgs *senderArgsPtr = args;
@@ -31,12 +42,13 @@ void *sender(void *args) {
 	// CITATION: http://beej.us/guide/bgnet/html/#getaddrinfoman
 	// CITATION: https://www.youtube.com/watch?v=MOrvead27B4
     struct addrinfo hints;
-	// struct addrinfo *results;   
 	struct addrinfo *result;    
 	struct sockaddr_in sinRemote;
 	int rv;
-	int socketDescriptor;
 	int len;
+	int socketDescriptor;
+
+	pthread_cleanup_push(senderCleanup, &socketDescriptor);
     
 	memset(&hints, 0, sizeof(hints));        
 	hints.ai_family = AF_INET;
@@ -49,39 +61,33 @@ void *sender(void *args) {
 
 	socketDescriptor = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
-
 	while(1) {
-//	    sleep(50);
         pthread_mutex_lock(&acceptingInputMutex);
 		if (List_count(senderList) < 1) {
 			pthread_cond_wait(&messagesToSend, &acceptingInputMutex);
 		}
-		char *temp = (char *) List_first(senderList);
-	
-		// int len = sendto(*(senderDataPtr->sd), (const char*) temp->item, MSG_MAX_LEN, 0, (const struct sockaddr *) senderDataPtr->serverAddress, sinLen);
-		len = sendto(socketDescriptor, (char *) temp, MSG_MAX_LEN, 0, result->ai_addr, result->ai_addrlen);
-		printf("Sent message %s\n", (char *) temp);
+		messageToSend = (char *) List_first(senderList);
+
+		len = sendto(socketDescriptor, (char *) messageToSend, MSG_MAX_LEN, 0, result->ai_addr, result->ai_addrlen);
 
 		List_remove(senderList); // Removing the first item on the list
 
-		if (*temp == '!') {
-		    pthread_cancel(inputThread);
-            pthread_cancel(receiverThread);
-		    pthread_cancel(displayThread);
-		    shutdown(socketDescriptor, 2);
-            pthread_cond_signal(&inputSpotAvailable);
-            pthread_mutex_unlock(&acceptingInputMutex);
+		if (*messageToSend == '!') {
+		    pthread_cancel(inputThread); // Sending a cancellation request to the input thread
+            pthread_cancel(receiverThread); // Sending a cancellation request to the receiver thread
+		    pthread_cancel(displayThread); // Sending a cancellation request to the display thread
 
-            return NULL;
+            pthread_exit(NULL); // Exiting the thread
 		}
 
-		free(temp); // Freeing the dynamically allocated char array
+		free(messageToSend); // Freeing the dynamically allocated char array
 
 		pthread_cond_signal(&inputSpotAvailable);
         pthread_mutex_unlock(&acceptingInputMutex);
 
 	}
-    return NULL;
+
+	pthread_cleanup_pop(1);
 }
 
 void senderInit(void *argv) {
@@ -89,7 +95,5 @@ void senderInit(void *argv) {
 }
 
 void senderDestructor() {
-    printf("In Sender Destructor");
     pthread_join(senderThread, NULL);
-    printf("Leaving Destructor");
 }
